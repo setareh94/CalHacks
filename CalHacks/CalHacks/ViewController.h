5a1ffb4f4b316f888b85da58de1dadf7e6dc3358
Setareh Lotfi <setarehlotfi94@hotmail.com>
2014-10-04 02:30:35 -0700

    udusid

--- CalHacks/CalHacks/ViewController.h
+++ CalHacks/CalHacks/ViewController.h
@@ -8,20 +8,8 @@
 
 #import <UIKit/UIKit.h>
 
+@interface ViewController : UIViewController
 
-@interface DeviceDetailViewController : UIViewController
-@property (weak, nonatomic) IBOutlet UITextField *nameTextField;
-@property (weak, nonatomic) IBOutlet UITextField *versionTextField;
-@property (weak, nonatomic) IBOutlet UITextField *companyTextField;
-
-@property (strong) NSManagedObject *device;
-
-
-- (IBAction)cancel:(id)sender;
-
-- (IBAction)save:(id)sender;
-
-@end
 
 @end
 
