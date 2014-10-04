3aa767d3281333ab30e1dd1843642ac0756237f1
lukelev07 <lukelev07@gmail.com>
2014-10-04 05:48:03 -0700

    stuff

---
+++ bootstrap/form_files/formoid1/form.php
@@ -0,0 +1,31 @@
+<?php
+
+define('EMAIL_FOR_REPORTS', '');
+define('RECAPTCHA_PRIVATE_KEY', '@privatekey@');
+define('FINISH_URI', 'http://');
+define('FINISH_ACTION', 'message');
+define('FINISH_MESSAGE', 'Your listing will appear on our homepage soon!');
+define('UPLOAD_ALLOWED_FILE_TYPES', 'doc, docx, xls, csv, txt, rtf, html, zip, jpg, jpeg, png, gif');
+
+require_once str_replace('\\', '/', __DIR__) . '/handler.php';
+
+?>
+
+<?php if (frmd_message()): ?>
+<link rel="stylesheet" href="<?=dirname($form_path)?>/formoid-solid-dark.css" type="text/css" />
+<span class="alert alert-success"><?=FINISH_MESSAGE;?></span>
+<?php else: ?>
+<!-- Start Formoid form-->
+<link rel="stylesheet" href="<?=dirname($form_path)?>/formoid-solid-dark.css" type="text/css" />
+<script type="text/javascript" src="<?=dirname($form_path)?>/jquery.min.js"></script>
+<form enctype="multipart/form-data" class="formoid-solid-dark" style="background-color:#FFFFFF;font-size:14px;font-family:'Roboto',Arial,Helvetica,sans-serif;color:#34495E;max-width:580px;min-width:150px" method="post"><div class="title"><h2>New Listing</h2></div>
+	<div class="element-input<?frmd_add_class("input")?>"><label class="title"><span class="required">*</span></label><div class="item-cont"><input class="large" type="text" name="input" required="required" placeholder="Title"/><span class="icon-place"></span></div></div>
+	<div class="element-textarea<?frmd_add_class("textarea")?>"><label class="title"><span class="required">*</span></label><div class="item-cont"><textarea class="medium" name="textarea" cols="20" rows="5" required="required" placeholder="Description"></textarea><span class="icon-place"></span></div></div>
+	<div class="element-file<?frmd_add_class("file")?>"><label class="title"><span class="required">*</span></label><div class="item-cont"><label class="large" ><div class="button">Choose Picture</div><input type="file" class="file_input" name="file" required="required"/><div class="file_text">No file selected</div><span class="icon-place"></span></label></div></div>
+	<div class="element-email<?frmd_add_class("email")?>"><label class="title"></label><div class="item-cont"><input class="large" type="email" name="email" value="" placeholder="Email"/><span class="icon-place"></span></div></div>
+<div class="submit"><input type="submit" value="Submit"/></div></form><script type="text/javascript" src="<?=dirname($form_path)?>/formoid-solid-dark.js"></script>
+
+<!-- Stop Formoid form-->
+<?php endif; ?>
+
+<?php frmd_end_form(); ?>
---
+++ bootstrap/js/handler.js
@@ -0,0 +1,7 @@
+/**
+ * Created by lukelev07 on 10/4/14.
+ */
+
+Parse.initialize("tjUwVLI6219JVwwhcry4HNBWMC60Q3xh8005NHBQ", "GxSaftLTUQ1itlVKSay4QNLkVafIiPrhVgHSqPA7");
+
+
